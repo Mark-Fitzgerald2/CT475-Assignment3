148c033534414625044b45d1eb76f21a814cbb50
Mark Fitzgerald <35224637+Mark-Fitzgerald2@users.noreply.github.com>
2018-12-02 22:16:27 +0000

    Made a change to how the matrix is set up

--- confusionMatrix.py
+++ confusionMatrix.py
@@ -6,24 +6,17 @@ class confusionMatrix(object):
 		matrix = []
 		unclassified = 0
 		for i in range(len(classNames)):
-			matrix.append([])
-
-		for j in range(len(classNames)):
-			for k in range(len(classNames)):
-				matrix[j].append([0])
+			matrix.append([0, 0, 0])
 
 		for x in range(len(output)):
 			
 			if pred[x] != 'N/A':
 				outIndex = classNames.index(output[x])
 				predIndex = classNames.index(pred[x])
-
-				val = list(matrix[outIndex][predIndex])
-				val[0] += 1
+				val = matrix[outIndex][predIndex]
+				val += 1
 				matrix[outIndex][predIndex] = val
 			else: 
 				unclassified += 1
 
 		return matrix, unclassified
-
-
